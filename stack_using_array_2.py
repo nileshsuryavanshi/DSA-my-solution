e4d3b8c30fdbdcffbc175232affafdc45f69fc5e
Nilesh Kumar Suryavanshi <77235287+nileshsuryavanshi@users.noreply.github.com>
2021-04-16 16:30:12 +0530

    Update stack_using_array_2.py

--- stack_using_array_2.py
+++ stack_using_array_2.py
@@ -1,51 +1,49 @@
 class Stack:
     def __init__(self, n):
-        # stack to store numerical value
         self.size = n 
-        self.stack = [None]*n  
-        self.top = -1
-
+        self.stack = [None]*n 
+        self.top = -1 
 
     def isEmpty(self):
-        # return True if stack is empty otherwise False
-        result = True if self.top == -1 else False
-        print(result)  
+        # return true if stack is empty otherwise false
+        return self.top == -1
 
-
-    def push(self, data):
-        # push element into stack
-        if self.top < self.size - 1:
-            self.top += 1
-            self.stack[self.top] = data 
-        else:
+    def push(self, val):
+        # if top == size-1 then show "stack overflow" else push element in stack 
+        if self.top == (self.size - 1):
             print('Stack Overflow')
-
+            return 
+        self.top += 1 
+        self.stack[self.top] = val 
 
     def pop(self):
-        # pop elemnt from stack
-        if self.top >= 0:
-            print('The popped element is :', self.stack[self.top])
-            self.top -= 1
-        else:
+        # if top == -1 then show "stack underflow" else pop the element
+        if self.top == -1:
             print('Stack Underflow')
-
+            return
+        popped = self.stack[self.top]
+        print('The popped element is:', popped)
+        self.top -= 1 
 
     def peek(self):
-        # return the last element in stack
+        # show the top element of stack
         if self.top == -1:
-            print(None)
-        else:    
-            print(self.stack[self.top])
-       
+            print('There is no element in stack')
+            return
+        print(self.stack[self.top])                
+
 
 st = Stack(5)
-st.isEmpty()
 st.push(11)
 st.push(12)
 st.push(13)
 st.push(14)
-st.peek()
-st.isEmpty()
 st.push(45)
 st.peek()
 st.push(88)
+st.pop()
+st.pop()
+st.pop()
+st.pop()
+st.pop()
+st.pop()
