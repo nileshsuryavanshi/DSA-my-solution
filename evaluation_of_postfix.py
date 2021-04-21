dd6d8425d35d6634c1f2de6a15913d2c701d5e60
Nilesh Kumar Suryavanshi <77235287+nileshsuryavanshi@users.noreply.github.com>
2021-04-21 11:43:28 +0530

    Update evaluation_of_postfix.py

--- evaluation_of_postfix.py
+++ evaluation_of_postfix.py
@@ -1,61 +1,71 @@
 class Stack:
-    def __init__(self):
-        # stack to store numerical value
-        self.stack = [] 
+    def __init__(self, n):
+        self.size = n 
+        self.stack = [None]*n 
+        self.top = -1 
 
 
     def isEmpty(self):
-        # return True if stack is empty otherwise False
-        return True if len(self.stack) == 0 else False  
+        # return true if stack is empty otherwise false
+        return self.top == -1
 
 
-    def push(self, data):
-        # push element into stack
-        self.stack.append(data)
+    def push(self, val):
+        # if top == size-1 then show "stack overflow" else push element in stack 
+        if self.top == (self.size - 1):
+            print('Stack Overflow')
+            return 
+        self.top += 1 
+        self.stack[self.top] = val 
 
 
     def pop(self):
-        # pop elemnt from stack
-        if self.isEmpty():
-            return 
-        return self.stack.pop()
+        # if top == -1 then show "stack underflow" else pop the element
+        if self.top == -1:
+            print('Stack Underflow')
+            return
+        popped = self.stack[self.top]
+        self.top -= 1 
+        return popped
 
 
     def peek(self):
-        # return the last element in stack
-        if self.isEmpty():
-            return 
-        return self.stack[-1]
+        # show the top element of stack
+        if self.top == -1:
+            return None 
+        else:
+            return self.stack[self.top]        
 
 
-    def calculate(self, s):
-        if len(self.stack) > 1:
-            first = int(self.pop())
-            second = int(self.pop())
-            if s == '+':
-                self.push(second + first)
-            elif s == '-':
-                self.push(second - first)
-            elif s == '*':
-                self.push(second * first)
-            elif s == '/':
-                self.push(second / first)
-            elif s == '^':
-                self.push(second ** first)
-                                
+    def calc(self, s):
+        # pop two elements and perform operation on them and push the result into stack
+        a = self.pop() 
+        b = self.pop()
+        if s == '+':
+            self.push(a+b) 
+        elif s == '-':
+            self.push(b-a) 
+        elif s == '*':
+            self.push(a*b) 
+        elif s == '/':
+            self.push(b/a)          
+        elif s == '^':
+            self.push(b**a)      
+
 
     def evalPostfix(self, string):
-        # splitting string into list
         string = string.split()
+        # iterate characters from string
         for s in string:
-            # if the value is digit then push it into stack
+            # if s is a number then push it directly
             if s.isdigit():
-                self.push(s)
-            # other wise do the calculation    
+                self.push(int(s))
+            # if s is operator then call calc method    
             else:
-                self.calculate(s)    
+                self.calc(s) 
+        print('Postfix evaluation :',self.peek())                               
 
 
-st = Stack()
+# initializing stack object 
+st = Stack(40)
 st.evalPostfix('100 200 + 2 / 5 * 7 +')
-print(st.peek())
